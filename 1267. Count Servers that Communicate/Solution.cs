5f9ae6160cbe535bde337ce49d3daeaac7ebaf28
OtajonovAri <otajonovmuhammadali668@gmail.com>
2025-01-23 23:47:31 +0500

    Daily Problem 1/23/2025 Server Gird Problem Level:Mediu

---
+++ 1267. Count Servers that Communicate/Solution.cs
@@ -0,0 +1,36 @@
+public class Solution {
+    public int CountServers(int[][] grid) {
+    int m = grid.Length;
+        int n = grid[0].Length; 
+
+       
+        int[] rowCount = new int[m];
+        int[] colCount = new int[n];
+
+        for (int i = 0; i < m; i++)
+        {
+            for (int j = 0; j < n; j++)
+            {
+                if (grid[i][j] == 1)
+                {
+                    rowCount[i]++; 
+                    colCount[j]++; 
+                }
+            }
+        }
+
+        int result = 0;
+        for (int i = 0; i < m; i++)
+        {
+            for (int j = 0; j < n; j++)
+            {
+                if (grid[i][j] == 1 && (rowCount[i] > 1 || colCount[j] > 1))
+                {
+                    result++; 
+                }
+            }
+        }
+
+        return result;
+    }
+}
