cd55544cf4d62b67f95849285d638a664359c43b
OtajonovAri <otajonovmuhammadali668@gmail.com>
2025-03-13 01:59:14 +0500

    Daily Problem 3/13/2025

---
+++ 2529. Maximum Count of Positive Integer and Negative Integer/Program.cs
@@ -0,0 +1,16 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.WriteLine("Hello, World!");
+
+
+public class Solution {
+    public bool CheckIfExist(int[] arr) 
+    {
+        for(int i = 0; i < arr.Length; i++)
+        {
+            int x = arr[i] * 2;
+            if(arr.Contains(x))
+               return true;
+        }    
+        return false;
+    }
+}
