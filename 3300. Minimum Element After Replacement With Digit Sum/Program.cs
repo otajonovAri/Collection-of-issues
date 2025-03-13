140a0a24b7fc5b170b01f54a5a0f75983cca0ce2
OtajonovAri <otajonovmuhammadali668@gmail.com>
2025-03-14 00:30:51 +0500

    Easy Problem Array Manipulation

---
+++ 3300. Minimum Element After Replacement With Digit Sum/Program.cs
@@ -0,0 +1,31 @@
+﻿Solution obj = new Solution();
+
+System.Console.WriteLine(obj.MinElement([10,12,13,14]));
+
+
+public class Solution {
+    public int MinElement(int[] nums) 
+    {
+        List<int> list = new List<int>();
+        int leng = nums.Length;
+        int index = 0;
+
+        while(leng != 0)
+        {
+            int x = nums[index];
+            int sum = 0;
+
+            while(x != 0)
+            {
+                int endNum = x % 10;
+                sum += endNum;
+                x /= 10;
+            }
+            list.Add(sum);
+            index++;
+            leng--;
+        }
+
+        return list.Min();
+    }
+}
