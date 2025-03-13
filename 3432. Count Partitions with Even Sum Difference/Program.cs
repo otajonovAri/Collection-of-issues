87f1a5114d3b6ce23ea9043007c0abcdb14c7da6
OtajonovAri <otajonovmuhammadali668@gmail.com>
2025-03-14 00:11:12 +0500

    Easy Problem Array Mainpulation

---
+++ 3432. Count Partitions with Even Sum Difference/Program.cs
@@ -0,0 +1,30 @@
+﻿Solution solution = new Solution();
+
+var arr = new int[]{10,10,3,7,6};
+
+int x = solution.CountPartitions(arr);
+
+
+public class Solution {
+    public int CountPartitions(int[] nums)
+    {
+        var count = 0;
+        var left = 0;
+        
+        for(var i = 0; i < nums.Length; i++)
+        {
+            var right = 0;            
+            left += nums[i];
+
+            for(var j = i + 1; j < nums.Length; j++)
+            {
+                right += nums[j];
+            }
+            
+             if(Math.Abs(left - right) % 2 == 0 && right != 0)
+               count++;
+        }
+
+        return count;
+    }
+}
